e4d3af0d215ab5279734e275bdee37a39e9e994b
Kallandor <dwilson@crystalfountains.com>
2014-08-13 16:51:30 -0400

    Start of the Build

---
+++ src/main/java/com/kallandor/mc_mod1/mc_mod1.java
@@ -0,0 +1,39 @@
+package com.kallandor.mc_mod1;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by dlwilson on 8/13/2014.
+ */
+@Mod(modid="mc_mod1", name="MC Mod1", version="1.7.10-1.0")
+
+
+public class mc_mod1 {
+
+    @Mod.Instance("mc_mod1")
+    public static MC_Mod instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLPostInitializationEvent event)
+    {
+
+    }
+
+}
+
+
