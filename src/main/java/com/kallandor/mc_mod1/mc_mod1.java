ea32b320fc01e54267d644efb2358f96034e2260
Kallandor <dwilson@crystalfountains.com>
2014-08-14 11:16:37 -0400

    Further adjustments to the template

--- src/main/java/com/kallandor/mc_mod1/mc_mod1.java
+++ src/main/java/com/kallandor/mc_mod1/mc_mod1.java
@@ -1,8 +1,10 @@
 package com.kallandor.mc_mod1;
 
+import com.kallandor.mc_mod1.configuration.ConfigurationHandler;
 import com.kallandor.mc_mod1.proxy.IProxy;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
+import com.kallandor.mc_mod1.reference.Reference;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
@@ -10,21 +12,21 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 /**
  * Created by dlwilson on 8/13/2014.
  */
-@Mod(modid="Reference.MOD_ID", name="Reference.MOD_NAME", version="Reference.VERSION")
+@Mod(modid= Reference.MOD_ID, name= Reference.MOD_NAME, version = Reference.VERSION)
 
 
 public class mc_mod1 {
 
-    @Mod.Instance("Reference.MOD_ID")
+    @Mod.Instance("mc_mod1")
     public static mc_mod1 instance;
 
-    @SidedProxy(clientSide = "com.kallandor.mc_mod1.proxy.ClientProxy", serverSide = "com.kallandor.mc_mod1.proxy.ServerProxy")
+    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
     public static IProxy proxy;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-
+        ConfigurationHandler.init(event.getSuggestedConfigurationFile());
     }
 
     @Mod.EventHandler
--- src/main/java/com/kallandor/mc_mod1/reference/Reference.java
+++ src/main/java/com/kallandor/mc_mod1/reference/Reference.java
@@ -8,5 +8,6 @@ public class Reference
     public static final String MOD_ID = "mc_mod1";
     public static final String MOD_NAME = "Minecraft Mod";
     public static final String VERSION = "1.7.10-1.0";
-
+    public static final String CLIENT_PROXY_CLASS = "com.kallandor.mc_mod1.proxy.ClientProxy";
+    public static final String SERVER_PROXY_CLASS = "com.kallandor.mc_mod1.proxy.ServerProxy";
 }
