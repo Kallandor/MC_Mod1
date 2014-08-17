31eb506a6ff10503ee3a8e5c8ab72f8ab3a63d92
Kallandor <dwilson@crystalfountains.com>
2014-08-16 20:38:43 -0400

    changes
    Conflicts:
    	MC_Mod1.iws
    	eclipse/logs/fml-junk-earlystartup.log
    	src/main/java/com/kallandor/mc_mod1/block/blockMCM.java

---
+++ src/main/java/com/kallandor/mc_mod1/block/BlockFlag.java
@@ -0,0 +1,13 @@
+package com.kallandor.mc_mod1.block;
+
+/**
+ * Created by dlwilson on 8/15/2014.
+ */
+public class BlockFlag extends blockMCM
+{
+    public BlockFlag()
+    {
+        super();
+        this.setBlockName("flag");
+    }
+}
---
+++ src/main/java/com/kallandor/mc_mod1/reference/Reference.java
@@ -0,0 +1,16 @@
+package com.kallandor.mc_mod1.reference;
+
+/**
+ * Created by dlwilson on 8/14/2014.
+ */
+public class Reference
+{
+    public static final String MOD_ID = "mc_mod1";
+    public static final String MOD_NAME = "Minecraft Mod";
+    public static final String VERSION = "1.7.10-1.0";
+    public static final String CLIENT_PROXY_CLASS = "com.kallandor.mc_mod1.proxy.ClientProxy";
+    public static final String SERVER_PROXY_CLASS = "com.kallandor.mc_mod1.proxy.ServerProxy";
+    public static final String GUI_FACTORY_CLASS = "com.kallandor.mc_mod1.client.gui.GuiFactory";
+
+
+}
